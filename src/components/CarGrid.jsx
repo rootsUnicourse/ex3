a2158b770dc27a79247a82eebd6821af773c7153
rootsUnicourse <rootsunicourse@gmail.com>
2025-01-26 19:10:22 +0200

    update carGrid

--- src/components/CarGrid.jsx
+++ src/components/CarGrid.jsx
@@ -6,9 +6,30 @@ import CarCard from "./CarCard";
 export default function CarGrid({ cars, favorites, onToggleFavorite }) {
     return (
         <Box sx={{ padding: 2 }}>
-            <Typography variant="h5" sx={{ marginBottom: 2 }}>
-                Cars ({cars.length})
+            <Typography variant="h5"
+                sx={{
+                    marginBottom: 2,
+                    fontWeight: 700,
+                    fontSize: 20,
+                    fontFamily: "'Plus Jakarta Sans', sans-serif",
+                    color: '#1A202C',
+                    
+                }}>
+                Cars Catalogue{" "}
+                <Typography
+                    component="span"
+                    sx={{
+                        fontFamily: "'Plus Jakarta Sans', sans-serif",
+                        fontWeight: 500,
+                        fontSize: 14,
+                        color: '#90A3BF',
+                        marginLeft: 3
+                    }}
+                >
+                    {cars.length} cars
+                </Typography>
             </Typography>
+
             <Box
                 sx={{
                     display: "flex",
